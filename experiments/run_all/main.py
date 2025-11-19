8cdce6408708b44368aeb55e08c1e720887b8ba8
hetvishastri <hetvishastri99@gmail.com>
2025-11-19 03:53:43 +0000

    run all experiments with correct naming and memory cleaning

--- experiments/run_all/main.py
+++ experiments/run_all/main.py
@@ -1,5 +1,3 @@
-
-# main.py
 import subprocess
 import json
 from config import tasks
@@ -23,23 +21,3 @@ if __name__ == "__main__":
                     "pipeline": p
                 })
             ])
-
-# from config import *
-# from inference_pipeline import InferencePipeline
-# import csv
-# import os
-# import json
-# if __name__ == "__main__":
-#     for task_name,task_info in tasks.items():
-#         print(f"Running task: {task_name}")
-#         for p in task_info['pipelines']:
-#             print(f"Running inference for model: {p['backbone']} on dataset: {task_info['datasets'][0]}")
-#             pipeline = InferencePipeline(task_name, task_info, p)
-#             pipeline.run()
- 
-#             #save results to a file
-#             # results_file = f"results_{model_name}_{task_name}.json"
-#             # with open(results_file, "w") as f:
-#             #     json.dump(results, f, indent=4)
-#             # print(f"Results for {model_name} on {task_name}")
-                    
