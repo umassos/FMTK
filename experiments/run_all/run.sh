d1c542d5ceb43b472ed8a729287f821f850d7e56
hetvishastri <hetvishastri99@gmail.com>
2026-03-09 00:33:33 +0000

    bash script for profiling in unity

---
+++ experiments/run_all/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mem=48G  # Requested Memory
+#SBATCH -p gpu,gpu-preempt # Partition
+#SBATCH --gres=gpu:a16:1 # Number and type of GPUs
+#SBATCH -t 08:00:00  # Job time limit
+#SBATCH -o ./logs/slurm-a16.out  # %j = job ID
+
+module load conda/latest
+conda activate fmtk_vlm
+python main.py combined_metrics_a16.csv
